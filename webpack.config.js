edaa6958ba8faeee7220d3de6e0c74ab3a2bc34a
wantunes <wantunes@stara.com.br>
2020-01-22 16:46:25 -0300

    Módulo 4 init

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,11 @@
-// // Descomentar para 2.1_Webpack_server
-// module.exports = {
-//     entry: './2.1_Webpack_server/src/main.js',
-//     output: {
-//         path: __dirname + '/2.1_Webpack_server/public',
-//         filename: 'bundle.js',
-//     },
-//     devServer: {
-//         contentBase: __dirname + '/2.1_Webpack_server/public',
-//     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.js$/,
-//                 exclude: /node_modules/,
-//                 use: {
-//                     loader: 'babel-loader',
-//                 }
-//             }
-//         ],
-//     },
-// };
-
 module.exports = {
-    entry: ['@babel/polyfill', './3.1_Async_await/src/main.js', './2.1_Webpack_server/src/main.js'],
+    entry: ['@babel/polyfill', './4.1_Aplicacao/src/main.js', './3.1_Async_await/src/main.js', './2.1_Webpack_server/src/main.js'],
     output: {
-        path: __dirname + '/3.1_Async_await/public',
+        path: __dirname + '/4.1_Aplicacao/public',
         filename: 'bundle.js',
     },
     devServer: {
-        contentBase: __dirname + '/3.1_Async_await/public',
+        contentBase: __dirname + '/4.1_Aplicacao/public',
     },
     module: {
         rules: [
